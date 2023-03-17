f711d52fe0942f67e808ff08b39affc62578a8a7
SuciPrasetiaNingrum <suciprasetianingrum30@gmail.com>
2023-03-17 14:45:46 +0700

    menambahkan file Calculator.cs

---
+++ Calculator.cs
@@ -0,0 +1,21 @@
+class Calculator
+{
+	static void Main(string[] args)
+	{
+		int a = 10;
+		
+		
+		
+		
+	}
+	static int penambahan(int a, int b)
+	{
+		return a + b;
+	}
+	
+	static int pengurangan(int a, int b)
+	{
+		return a - b;
+	}
+	
+}
